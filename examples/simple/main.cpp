bfc7ccda4c44f1b4899057339591057412539e8b
Kiju <dev@jakubkijek.com>
2026-03-16 14:40:20 +0100

    feat: type name display

--- examples/simple/main.cpp
+++ examples/simple/main.cpp
@@ -1,3 +1,6 @@
+#include <string>
+#include <vector>
+
 #include "../../include/serialization.hpp"
 
 struct Position {
@@ -5,8 +8,8 @@ struct Position {
     float y {};
     float z {};
 
-    template <ser::IsArchiveT Archive>
-    void serialize(Archive& ar) {
+    template <ser::IsArchiveT ArchiveT>
+    void serialize(ArchiveT& ar) {
         ar.property("x", x);
         ar.property("y", y);
         ar.property("z", z);
@@ -37,13 +40,37 @@ struct Transform {
     }
 };
 
-int main() {
-    ser::DebugWriter debug_writer;
-    Position position { .x = 1.0f, .y = -1.0f, .z = 42.42f };
-    Rotation rotation { .yawn = 1.0f, .pitch = 0.5f, .roll = 0.69f };
-    Transform transform { .position = position, .rotation = rotation };
+struct State {
+    int version { 0 };
+    std::string name {};
+    std::vector<Transform> transforms {};
 
-    transform.serialize(debug_writer);
+    template <ser::IsArchiveT ArchiveT>
+    void serialize(ArchiveT& ar) {
+        ar.property("version", version);
+        ar.property("name", name);
+        ar.property("transforms", transforms);
+    }
+};
+
+int main() {
+    ser::DebugStdoutWriter debug_stdout_writer;
+
+    Position position_a { .x = 1.0f, .y = -1.0f, .z = 42.42f };
+    Rotation rotation_a { .yawn = 1.0f, .pitch = 0.5f, .roll = 0.69f };
+    Transform transform_a { .position = position_a, .rotation = rotation_a };
+
+    Position position_b { .x = 67.67f, .y = -42.0f, .z = 42.42f };
+    Rotation rotation_b { .yawn = 3.3f, .pitch = 1.5f, .roll = 42.69f };
+    Transform transform_b { .position = position_b, .rotation = rotation_b };
+
+    State state {
+        .version = 0,
+        .name    = "Game of Life",
+        .transforms { transform_a, transform_b },
+    };
+
+    state.serialize(debug_stdout_writer);
 
     return 0;
 }
--- include/serialization.hpp
+++ include/serialization.hpp
@@ -7,6 +7,7 @@
 #include <string>
 #include <string_view>
 #include <type_traits>
+#include <vector>
 
 namespace ser {
 
@@ -26,42 +27,138 @@ using F32 = float;
 using F64 = double;
 
 using Byte = std::byte;
+using Bool = bool;
 
 enum class ArchiveMode { Read, Write };
 
 template <typename ArchiveT>
-concept IsArchiveT =
-    requires(ArchiveT archive, U32& value) { archive.property("property_name", value); };
+concept IsArchiveT = requires(ArchiveT archive, U32& value) {
+    { ArchiveT::mode } -> std::same_as<const ArchiveMode&>;
+    { archive.property("property_name", value) } -> std::same_as<void>;
+};
 
 template <typename ArchiveT, typename ValueT>
-concept HasMemberSerialize =
+concept HasMemberSerializeT =
     requires(ArchiveT& archive, ValueT& value) { value.serialize(archive); };
 
 template <typename ArchiveT, typename ValueT>
-concept HasGlobalSerialize =
-    requires(ArchiveT& archive, ValueT& value) { serialize(archive, value); };
+concept HasGlobalSerializeT =
+    requires(ArchiveT& archive, ValueT& value) { serialize_global(archive, value); };
 
 template <typename ArchiveT, typename ValueT>
-concept IsSerializable = HasGlobalSerialize<ArchiveT, ValueT>;
+concept IsSerializableT =
+    HasMemberSerializeT<ArchiveT, ValueT> || HasGlobalSerializeT<ArchiveT, ValueT>;
 
-class DebugWriter {
-    U32 m_depth { 0 };
+// WARNING:
+// This one was written by a Clanker. It was not tested on windows with MSVC.
+template <typename T>
+consteval std::string_view compt_get_type_name() {
+#if defined(__clang__) || defined(__GNUC__)
+    std::string_view name = __PRETTY_FUNCTION__;
+    // Extract the type from consteval std::string_view
+    // "compt_get_type_name() [with T = TypeName]"
+    size_t start = name.find("T = ") + 4;
+    size_t end   = name.find_last_of(']');
+    return name.substr(start, end - start);
+#elif defined(_MSC_VER)
+    std::string_view name = __FUNCSIG__;
+    // Extract the type from
+    // "class std::basic_string_view<char,struct std::char_traits<char>>
+    //  __cdecl compt_get_type_name<TypeName>(void)"
+    size_t start = name.find("compt_get_type_name<") + 12;
+    size_t end   = name.find_last_of('>');
+    return name.substr(start, end - start);
+#else
+    return "UNKNOWN";
+#endif
+}
 
+class DebugStdoutWriter {
    public:
+    static constexpr ArchiveMode mode = ArchiveMode::Write;
+    static constexpr U32 tab_width   = 4;
+
     template <typename ValueT>
     void property(std::string_view name, ValueT& value) {
-        if constexpr (HasMemberSerialize<DebugWriter, ValueT>) {
-            std::println("{}{}:", std::string(m_depth, '\t'), name);
+        constexpr std::string_view compt_type_name = compt_get_type_name<ValueT>();
+
+        if constexpr (IsSerializableT<DebugStdoutWriter, ValueT>) {
+            std::println("{}[{}] {}:", std::string(m_depth * tab_width, ' '),
+                         compt_type_name, name);
             ++m_depth;
-            value.serialize(*this);
+
+            if constexpr (HasMemberSerializeT<DebugStdoutWriter, ValueT>) {
+                value.serialize(*this);
+            } else {
+                serialize_global(*this, value);
+            }
+
             --m_depth;
+        } else if constexpr (std::is_same_v<ValueT, U8>) {
+            std::println("{}[{}] {}: {}", std::string(m_depth * tab_width, ' '),
+                         compt_type_name, name, static_cast<U32>(value));
+        } else if constexpr (std::is_same_v<ValueT, U16>) {
+            std::println("{}[{}] {}: {}", std::string(m_depth * tab_width, ' '),
+                         compt_type_name, name, value);
         } else if constexpr (std::is_same_v<ValueT, U32>) {
-            std::println("{}{}: {}", std::string(m_depth, '\t'), name, value);
+            std::println("{}[{}] {}: {}", std::string(m_depth * tab_width, ' '),
+                         compt_type_name, name, value);
+        } else if constexpr (std::is_same_v<ValueT, U64>) {
+            std::println("{}[{}] {}: {}", std::string(m_depth * tab_width, ' '),
+                         compt_type_name, name, value);
+        } else if constexpr (std::is_same_v<ValueT, S8>) {
+            std::println("{}[{}] {}: {}", std::string(m_depth * tab_width, ' '),
+                         compt_type_name, name, static_cast<S32>(value));
+        } else if constexpr (std::is_same_v<ValueT, S16>) {
+            std::println("{}[{}] {}: {}", std::string(m_depth * tab_width, ' '),
+                         compt_type_name, name, value);
+        } else if constexpr (std::is_same_v<ValueT, S32>) {
+            std::println("{}[{}] {}: {}", std::string(m_depth * tab_width, ' '),
+                         compt_type_name, name, value);
+        } else if constexpr (std::is_same_v<ValueT, S64>) {
+            std::println("{}[{}] {}: {}", std::string(m_depth * tab_width, ' '),
+                         compt_type_name, name, value);
+        } else if constexpr (std::is_same_v<ValueT, USize>) {
+            std::println("{}[{}] {}: {}", std::string(m_depth * tab_width, ' '),
+                         compt_type_name, name, value);
         } else if constexpr (std::is_same_v<ValueT, F32>) {
-            std::println("{}{}: {}", std::string(m_depth, '\t'), name, value);
+            std::println("{}[{}] {}: {}", std::string(m_depth * tab_width, ' '),
+                         compt_type_name, name, value);
+        } else if constexpr (std::is_same_v<ValueT, F64>) {
+            std::println("{}[{}] {}: {}", std::string(m_depth * tab_width, ' '),
+                         compt_type_name, name, value);
+        } else if constexpr (std::is_same_v<ValueT, Bool>) {
+            std::println("{}[{}] {}: {}", std::string(m_depth * tab_width, ' '),
+                         compt_type_name, name, value ? "true" : "false");
+        } else if constexpr (std::is_same_v<ValueT, std::string>) {
+            std::println("{}[{}] {}: {}", std::string(m_depth * tab_width, ' '),
+                         compt_type_name, name, value);
+        } else if constexpr (std::is_same_v<ValueT, std::string_view>) {
+            std::println("{}[{}] {}: {}", std::string(m_depth * tab_width, ' '),
+                         compt_type_name, name, value);
+        } else if constexpr (std::is_same_v<ValueT, Byte>) {
+            std::println("{}[{}] {}: 0x{:02X}", std::string(m_depth * tab_width, ' '),
+                         compt_type_name, name, std::to_integer<U32>(value));
         }
     }
+
+   private:
+    U32 m_depth { 0 };
 };
 
-class DebugReader {};
+template <IsArchiveT ArchiveT, typename ValueT>
+void serialize_global(ArchiveT& ar, std::vector<ValueT>& vec) {
+    USize size = vec.size();
+
+    ar.property("size", size);
+
+    if constexpr (ArchiveT::mode == ArchiveMode::Read) {
+        vec.resize(size);
+    }
+
+    for (USize i = 0; i < size; ++i) {
+        ar.property("item." + std::to_string(i), vec[i]);
+    }
+}
+
 }  // namespace ser
